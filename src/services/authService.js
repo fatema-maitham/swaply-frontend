550f5e42f90ee010953b084d31f50cd58a2ae872
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-15 15:58:35 +0300

    / removal for deployment

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}auth`;
 
 const getUserFromToken = (token) => {
   const payload = token.split('.')[1];
